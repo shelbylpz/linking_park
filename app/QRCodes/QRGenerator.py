159da7d8d5c36643a93d6cc47184eb2c4645970c
Alberto Demian López Vázquez <alberto.lopez8744@alumnos.udg.mx>
2023-05-19 10:32:46 -0600

    QRGenerator Fix 5.19.1

--- app/QRCodes/QRGenerator.py
+++ app/QRCodes/QRGenerator.py
@@ -4,12 +4,14 @@ import os, shutil
 from pyqrcode import QRCode
 import time
 
+route = os.path.abspath(os.getcwd())
+
 def generator(id):
     qr = pyqrcode.create(str(id), error='L')
     qr.png(str(id)+'.png', scale = 6)
-    shutil.move('./'+str(id)+'.png','./app/QRCodes/img/')
+    shutil.move(str(route)+'/'+str(id)+'.png',str(route)+'/app/QRCodes/img/')
     
 def generator_after_out(id):
     qr = pyqrcode.create(str(id), error='L')
     qr.png(str(id)+'.png', scale = 6)
-    shutil.move('./'+str(id)+'.png','./app/QRCodes/img/')
+    shutil.move(str(route)+'/'+str(id)+'.png',str(route)+'/app/QRCodes/img/')
