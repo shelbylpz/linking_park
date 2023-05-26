895efa7c3994d8570cccd1d1fcb2c9b4fd19853b
Alberto Demian López Vázquez <115967900+AlbertoLopez8744@users.noreply.github.com>
2023-05-25 21:07:04 -0600

    Actualizar lectorValida.py

--- lectorValida.py
+++ lectorValida.py
@@ -9,15 +9,7 @@ import numpy as np
 import time
 
 
-def conectar_db():
-    conexion = psycopg2.connect(
-        user = 'postgres',
-        password = '22042003-a',
-        host = 'azure-flask-dbapp.postgres.database.azure.com',
-        port = '5432',
-        database = 'LinkingParkDB'
-    )
-    return conexion
+
 
 # Creamos la videocaptura
 cap = cv2.VideoCapture(0)
