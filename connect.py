e3c3a74c403cd59c49f8657ee4f47c0d94a9ecb6
César <ipog71@gmail.com>
2023-09-02 19:34:54 -0600

    formato

--- connect.py
+++ connect.py
@@ -2,6 +2,10 @@ import psycopg2
 
 def connect():
     conn = psycopg2.connect(
-      database="linking_park", user='postgres', password='RTvAsfCAv3neSn', host='serverproyectoxdbbdd.postgres.database.azure.com ', port= '5432'
+      database="linking_park", 
+      user='postgres', 
+      password='RTvAsfCAv3neSn', 
+      host='serverproyectoxdbbdd.postgres.database.azure.com ', 
+      port= '5432'
    )
     return conn
